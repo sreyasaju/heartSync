65a00ea1c9c16c502e0803a66f7eb101b9666d29
Sreya Saju <68282466+sreyasaju@users.noreply.github.com>
2025-07-24 00:26:44 +0300

    update medication logger button

--- lib/pages/medication_logger.dart
+++ lib/pages/medication_logger.dart
@@ -2,8 +2,6 @@ import "package:flutter/material.dart";
 import 'package:heart_sync/components/side_menu.dart';
 import 'package:heart_sync/components/nav_bar.dart';
 
-
-
 class MedicationLogger extends StatefulWidget {
   const MedicationLogger({super.key});
 
@@ -12,17 +10,31 @@ class MedicationLogger extends StatefulWidget {
 }
 
 class _MedicationLoggerState extends State<MedicationLogger> {
+  final _formGlobalKey = GlobalKey<FormState>();
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text("Add Medication"),
-        ),
-      extendBody: true,
       backgroundColor: Colors.grey[300],
       drawer: const SideMenu(),
       bottomNavigationBar: const NavBar(),
+      floatingActionButton: FloatingActionButton(
+        backgroundColor: Colors.pink[900],
+        foregroundColor: Colors.pink[50],
+        onPressed: () {
+          // Respond to button press
+        },
+        child: const Icon(Icons.add),
+      ),
+      body: SafeArea(
+        child: SingleChildScrollView(
+          physics: const BouncingScrollPhysics(),
+          child: Column(
+            children: <Widget>[
+              // your other widgets here
+            ],
+          ),
+        ),
+      ),
     );
-    
   }
 }
