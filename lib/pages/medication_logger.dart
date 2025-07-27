b0b505fb2a9ea7901929710958cf8107fe085d97
Sreya Saju <68282466+sreyasaju@users.noreply.github.com>
2025-07-27 04:06:44 +0300

    fix: meds page

--- lib/pages/medication_logger.dart
+++ lib/pages/medication_logger.dart
@@ -15,6 +15,12 @@ class _MedicationLoggerState extends State<MedicationLogger> {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.grey[300],
+      appBar: AppBar(
+        title: const Text('Meds'),
+        backgroundColor: Colors.pink[900],
+        foregroundColor: Colors.pink[50],
+        elevation: 2,
+      ),
       drawer: const SideMenu(),
       bottomNavigationBar: const NavBar(),
       body: Stack(
@@ -24,20 +30,26 @@ class _MedicationLoggerState extends State<MedicationLogger> {
               physics: const BouncingScrollPhysics(),
               child: Column(
                 children: <Widget>[
-                  // your other widgets here
+                
                 ],
               ),
             ),
           ),
           Align(
             alignment: Alignment.center,
-            child: FloatingActionButton(
-              backgroundColor: Colors.pink[900],
-              foregroundColor: Colors.pink[50],
-              onPressed: () {
-                // Respond to button press
-              },
-              child: const Icon(Icons.add),
+            child: SizedBox(
+              width: 90, 
+              height: 90,
+              child: FloatingActionButton(
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(60),
+                ),
+                backgroundColor: Colors.pink[900],
+                foregroundColor: Colors.pink[50],
+                onPressed: () {
+                    },
+                child: const Icon(Icons.add, size: 48),
+              ),
             ),
           ),
         ],
